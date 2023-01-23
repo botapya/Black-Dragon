3aeb268bf4b293707bc757cff39c5788b7dee2e7
botapya <122734310+botapya@users.noreply.github.com>
2023-01-23 19:30:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
+global.owner = ['94723921905'] //ur owner number
+global.ownername = "Hashan Official" //ur owner name
+global.ytname = "YT: Hasha bot" //ur yt chanel name
+global.socialm = "GitHub: hashaofficial" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
-global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
+global.ownernomer = "94723921905" //ur number
+global.premium = ['94723921905'] //ur premium number
+global.botname = 'Hashan bot' //ur bot name
+global.linkz = "https://chat.whatsapp.com/H2cnokTxloD3aNv3M6UJoz" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.hashan.ga" //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/H2cnokTxloD3aNv3M6UJoz' //script link
 global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.packname = "Sticker By Hashan bot" //ur sticker watermark packname
+global.author = "Hashan  Official" //ur sticker watermark author
+global.wm = "𝙃𝘼𝙎𝙃𝘼𝙉 𝘽𝙊𝙏" //ur watermark
 
 // Other
 global.sessionName = 'session'
